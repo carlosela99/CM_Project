afdd89cb9aa00315468cd2a9843da691b710850d
Carlos Antunes <190221099@estudantes.ips.pt>
2021-04-23 00:36:53 +0100

    Input text style

--- TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/Welcome.java
+++ TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/Welcome.java
@@ -2,7 +2,9 @@ package com.example.reclaimportugal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void showLogin(View v){
+        startActivity(new Intent(Welcome.this, Login.class));
     }
 }
