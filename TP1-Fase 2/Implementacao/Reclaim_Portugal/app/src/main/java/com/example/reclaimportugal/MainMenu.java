c8daa6badc5c7a3ba2613bc74ae045b6c678b652
Ricardo <190221060@estudantes.ips.pt>
2021-04-24 16:55:05 +0100

    MainMenu Progress

--- TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/MainMenu.java
+++ TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/MainMenu.java
@@ -1,17 +1,32 @@
 package com.example.reclaimportugal;
 
+import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        imageButton = (ImageButton) findViewById(R.id.settings_icon);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettings();
+            }
+        });
+    }
+
+    public void openSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
--- TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/Welcome.java
+++ TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/Welcome.java
@@ -2,13 +2,30 @@ package com.example.reclaimportugal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openMainMenu();
+            }
+        });
+    }
+
+    public void openMainMenu() {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 }
