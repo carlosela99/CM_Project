9944bdf40d08612ffae874efd63ec671c7f7da29
Jorge Espiguinha Mimoso <jemimoso@gmail.com>
2021-05-16 00:21:13 +0100

    Bussola adicionada

--- TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/MapsActivity.java
+++ TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/MapsActivity.java
@@ -31,10 +31,31 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
 GoogleApiClient.OnConnectionFailedListener,GoogleMap.InfoWindowAdapter,
 LocationListener{
+
+    private ImageView imageView;
+
+    private SensorManager sensorManager;
+    private Sensor sensorAccelerometer;
+    private Sensor sensorMagneticField;
+
+    private float[] floatGravity = new float[3];
+    private float[] floatGeoMagnetic = new float[3];
+
+    private float[] floatOrientation = new float[3];
+    private float[] floatRotationMatrix = new float[9];
 
 
     private GoogleMap mMap;
@@ -63,6 +84,51 @@ LocationListener{
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+        imageView = findViewById(R.id.back_region2);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        SensorEventListener sensorEventListenerAccelrometer = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                floatGravity = event.values;
+
+                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+                SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+
+                imageView.setRotation((float) (-floatOrientation[0]*180/3.14159));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+
+        SensorEventListener sensorEventListenerMagneticField = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                floatGeoMagnetic = event.values;
+
+                SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+                SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+
+                imageView.setRotation((float) (-floatOrientation[0]*180/3.14159));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+        sensorManager.registerListener(sensorEventListenerAccelrometer, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListenerMagneticField, sensorMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+    public void ResetButton(View view){
+        imageView.setRotation(180);
     }
 
     @Override
