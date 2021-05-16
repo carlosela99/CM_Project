8a22fd0658662967616fb3a151494dbca3aa7a64
Carlos Antunes <190221099@estudantes.ips.pt>
2021-05-17 00:00:03 +0100

    Splash screen

---
+++ TP1-Fase 2/Implementacao/Reclaim_Portugal/app/src/main/java/com/example/reclaimportugal/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.example.reclaimportugal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import static java.lang.Thread.sleep;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        image = findViewById(R.id.icon);
+
+        ScaleAnimation fade_in =  new ScaleAnimation(1f, 1.2f, 1f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        fade_in.setDuration(3000);
+        fade_in.setFillAfter(true);
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashScreen.this, Welcome.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        }).start();
+
+        image.startAnimation(fade_in);
+    }
+}
